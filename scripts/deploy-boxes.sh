521ce9c62b077900d9c857352e9387d03d9ea48e
githubfoam <lequestionis@gmail.com>
2020-06-24 23:51:28 +0300

    магнетизмом

--- scripts/deploy-boxes.sh
+++ scripts/deploy-boxes.sh
@@ -9,8 +9,8 @@ echo "==========================================================================
 vagrant plugin install vagrant-libvirt #The vagrant-libvirt plugin is required when using KVM on Linux
 vagrant plugin install vagrant-mutate #Convert vagrant boxes to work with different providers
 
-vagrant box add "bento/ubuntu-20.04" --provider=virtualbox
-vagrant mutate "bento/ubuntu-20.04" libvirt
+vagrant box add "bento/ubuntu-19.10" --provider=virtualbox
+vagrant mutate "bento/ubuntu-19.10" libvirt
 vagrant up --provider=libvirt
 
 
