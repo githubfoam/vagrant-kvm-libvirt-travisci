4d4b9e1a5c551b6e70b1050b9931f9ed2651ff1d
githubfoam <lequestionis@gmail.com>
2020-06-24 20:35:24 +0300

    исходи́ть из чего

--- scripts/deploy-vagrant.sh
+++ scripts/deploy-vagrant.sh
@@ -9,15 +9,13 @@ echo "=============================Install Vagrant==============================
 # export VAGRANT_CURRENT_VERSION="2.2.9"
 apt-get install -qqy unzip jq
 
-# https://www.vagrantup.com/downloads.html
 # https://releases.hashicorp.com/vagrant/2.2.9/vagrant_2.2.9_SHA256SUMS
 export VAGRANT_CURRENT_VERSION="$(curl -s https://checkpoint-api.hashicorp.com/v1/check/vagrant | jq -r -M '.current_version')"
 export VAGRANT_URL="https://releases.hashicorp.com/vagrant/${VAGRANT_CURRENT_VERSION}/vagrant_${VAGRANT_CURRENT_VERSION}_x86_64.deb"
 export VAGRANT_SHA256_URL="https://releases.hashicorp.com/vagrant/$VAGRANT_CURRENT_VERSION/vagrant_${VAGRANT_CURRENT_VERSION}_SHA256SUMS"
 export  VAGRANT_SHA256_SIG_URL="https://releases.hashicorp.com/vagrant/$VAGRANT_CURRENT_VERSION/vagrant_${VAGRANT_CURRENT_VERSION}_SHA256SUMS.sig"
 
-# wget -nv https://releases.hashicorp.com/vagrant/${VAGRANT_CURRENT_VERSION}/vagrant_${VAGRANT_CURRENT_VERSION}_x86_64.deb
-
+# wget -nv "${VAGRANT_URL}"
 curl -LO "${VAGRANT_URL}"
 curl -LO "${VAGRANT_SHA256_URL}"
 curl -LO "${VAGRANT_SHA256_SIG_URL}"
@@ -25,8 +23,15 @@ export HASHICORP_PUBLIC_KEY_URL="https://keybase.io/hashicorp/pgp_keys.asc" #htt
 # curl -sSL "${HASHICORP_PUBLIC_KEY_URL}" | gpg --import - # import the public key (PGP key)
 `curl -sSL "${HASHICORP_PUBLIC_KEY_URL}" | gpg --import -` # import the public key (PGP key)
 gpg --verify "vagrant_${VAGRANT_CURRENT_VERSION}_SHA256SUMS.sig" "vagrant_${VAGRANT_CURRENT_VERSION}_SHA256SUMS" 2>/dev/null #Verify the signature file is untampered
-shasum -a 256 -c "vagrant_${VAGRANT_CURRENT_VERSION}_SHA256SUMS" | sudo tee output.txt  # Verify the SHASUM matches the archive.
-cat output.txt  | grep OK # print OK
+
+sha256sum  vagrant_${VAGRANT_CURRENT_VERSION}_x86_64.deb # via sha256sum
+openssl dgst -sha256 vagrant_${VAGRANT_CURRENT_VERSION}_x86_64.deb # via openssl
+
+shasum -a 256 -c "vagrant_${VAGRANT_CURRENT_VERSION}_SHA256SUMS"
+# shasum -a 256 -c "vagrant_${VAGRANT_CURRENT_VERSION}_SHA256SUMS" | sudo tee output.txt  # Verify the SHASUM matches the archive.
+# cat output.txt  | grep OK # print OK
+
+
 
 dpkg -i vagrant_${VAGRANT_CURRENT_VERSION}_x86_64.deb
 vagrant version
